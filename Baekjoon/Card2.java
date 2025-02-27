51c608684d4aa7547c21d1b67923df2a642aa574
nam-kiyun <fordnam1@gmail.com>
2025-02-27 19:32:55 +0900

    초기 파일 업로드

---
+++ Baekjoon/Card2.java
@@ -0,0 +1,12 @@
+package Baekjoon;
+import java.io.*;
+import java.util.*;
+
+public class Card2 {
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
+
+
+    }
+}
